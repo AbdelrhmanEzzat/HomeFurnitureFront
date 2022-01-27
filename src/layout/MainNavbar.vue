c7fcc60955d0278d74241767e94d70f3ccd7faa3
fatma.fatmssalah04 <fatma.fatmssalah04@gmail.com>
2022-01-27 13:17:33 +0200

    commit4

--- src/layout/MainNavbar.vue
+++ src/layout/MainNavbar.vue
@@ -62,11 +62,12 @@
                   </div>
                 </a>
               </li> -->
-              <md-list-item
-                href="/#/shop"
-                @click="scrollToElement()"
-                v-if="showDownload"
-              >
+              <md-list-item href="/#/home" target="_blank" v-if="showDownload">
+                <i class="material-icons">home</i>
+                <p>Home</p>
+              </md-list-item>
+
+              <md-list-item href="/#/shop" target="_blank" v-if="showDownload">
                 <i class="material-icons">store</i>
                 <p>Store</p>
               </md-list-item>
