d6ecd1fe182c3e3e05a468f5e62fabf700c5a12e
AbdelrhmanEzzat <64223277+AbdelrhmanEzzat@users.noreply.github.com>
2022-01-30 01:10:12 +0200

    menu open in same page

--- src/layout/MainNavbar.vue
+++ src/layout/MainNavbar.vue
@@ -62,12 +62,12 @@
                   </div>
                 </a>
               </li> -->
-              <md-list-item href="/#/home" target="_blank" v-if="showDownload">
+              <md-list-item href="/#/home"  v-if="showDownload">
                 <i class="material-icons">home</i>
                 <p>Home</p>
               </md-list-item>
 
-              <md-list-item href="/#/shop" target="_blank" v-if="showDownload">
+              <md-list-item href="/#/shop"  v-if="showDownload">
                 <i class="material-icons">store</i>
                 <p>Store</p>
               </md-list-item>
